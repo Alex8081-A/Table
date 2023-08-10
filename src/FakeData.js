4c1c94c304704e3b6b68fcae34cf9f575edd1c7a
Alex8081-A <vladaleksiyevich@gmail.com>
2023-08-10 13:17:41 +0300

    Finish

---
+++ src/FakeData.js
@@ -0,0 +1,51 @@
+export const UsersData = [
+    {
+      id: 1,
+      name: "Goncharov Dmitry",
+      birthday: "09.02.2001",
+      grade: 1,
+    },
+    {
+      id: 2,
+      name: "Gromova Alina",
+      birthday: "11.03.2000",
+      grade: 2,
+    },
+    {
+      id: 3,
+      name: "Fedotova Veronika",
+      birthday: "19.11.2001",
+      grade: 3,
+    },
+    {
+      id: 4,
+      name: "Sviridova Kira",
+      birthday: "03.01.2000",
+      grade: 3,
+    },
+    {
+      id: 5,
+      name: "Lavrov Egor",
+      birthday: "12.07.2001",
+      grade: 5,
+    },
+    {
+      id: 6,
+      name: "Ageev Makar",
+      birthday: "09.12.2001",
+      grade: 10,
+    },
+    {
+      id: 7,
+      name: "Karasev Aleksey",
+      birthday: "29.05.2001",
+      grade: 9,
+    },
+    {
+      id: 8,
+      name: "Selivanov Stepan",
+      birthday: "18.03.2000",
+      grade: 1,
+    },
+  ];
+  
