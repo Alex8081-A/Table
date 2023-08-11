4466e6559f1fc815068760bd54a0c11c044c985e
Alex8081-A <116066845+Alex8081-A@users.noreply.github.com>
2023-08-11 12:54:38 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 
 import usersReducer from './features/Users';
-// console.log(usersReducer);
 
 const store = configureStore({
   reducer: {
